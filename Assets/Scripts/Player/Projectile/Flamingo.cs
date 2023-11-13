245b9edd093b3a4d1f9d56f53529b580d533a76b
rhtjdwns <64015904+rhtjdwns@users.noreply.github.com>
2023-11-13 15:35:43 +0900

    222

--- Assets/Scripts/Player/Projectile/Flamingo.cs
+++ Assets/Scripts/Player/Projectile/Flamingo.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Flamingo : MonoBehaviour
 {
-    Player player;
+    public AnimationCurve SwishCurve;
     public Vector3 initPosition;
+    Transform startPosition;
+    Player player;
+    float time = 0;
 
     void Start()
     {
-        player = GetComponentInParent<Player>();
-        transform.position = new Vector3(player.transform.position.x + 3f, player.transform.position.y);
-        initPosition = new Vector3(player.transform.position.x + 0.5f, player.transform.position.y);
     }
 
     void Update()
     {
-        transform.RotateAround(initPosition, Vector3.back, 2);
+        SwishFlamingo();
+    }
+
+    void SwishFlamingo()
+    {
+        time += Time.deltaTime;
+        transform.RotateAround(initPosition, Vector3.back, SwishCurve.Evaluate(time));
+
+        if (SwishCurve.Evaluate(time) == 0)
+        {
+            transform.position = startPosition.position;
+            transform.rotation = startPosition.rotation;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        player = GetComponentInParent<Player>();
+        transform.position = new Vector3(player.transform.position.x + 3f, player.transform.position.y + 2.2f);
+        startPosition = transform;
+        initPosition = new Vector3(player.transform.position.x + 0.5f, player.transform.position.y);
+        time = 0;
     }
 }
